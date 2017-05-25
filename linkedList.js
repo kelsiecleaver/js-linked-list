6bfb5576ca5cc609036dde7c9d1f4c4fd5f789d8
Kelsie Cleaver <kelsiecleaver@Kelsies-MacBook-Pro.local>
2017-05-24 20:53:43 -1000

    getTail

--- linkedList.js
+++ linkedList.js
@@ -4,5 +4,46 @@
  * @return {Object} an object exposing methods to be used to manipulate a linked list
  */
 function linkedListGenerator(){
+var head = null;
+var tail = null;
+
+function getHead(){
+  return head;
+
+}
+function getTail(){
+  return tail;
+}
+function add(value){
+  if(head === null){
+    head = {
+    value: value,
+    next: null,
+    };
+    tail = head;
+  }else{
+
+
+  }
+
+}
+function get(number){
+
+}
+function remove(){
+
+}
+function insert(){
+
+}
+return{
+  getHead: getHead,
+  getTail: getTail,
+  add: add,
+  get: get,
+  remove: remove,
+  insert: insert
+
+};
 
 }
